c78ed54504ff74f20e5584b94a14462c2ae6390b
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-22 18:17:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function toCase(text) {
   // write your code here
+	return text.toUpperCase(); 
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
+ const text = prompt("Enter text:");
 alert(toCase(text));
